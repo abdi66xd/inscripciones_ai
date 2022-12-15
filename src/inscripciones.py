4816e13102a7b1019728455e33c6a038fc6234e0
abdi66xd <abdiasalpire12@gmail.com>
2022-12-14 20:17:38 -0400

    random and logic changes

--- src/inscripciones.py
+++ src/inscripciones.py
@@ -51,8 +51,9 @@ nombre_cajero = nombre_cajero.replace(nombre_cajero[len(nombre_cajero) - 1:], st
 nombre_director = nombre_director.replace(nombre_director[len(nombre_director) - 1:], str(random.randint(1, 10)))
 semestre = siguiente_semestre(semestre)
 tiempo_habilitacion_materias = random.randint(60, 120)
-
-
+billeteraRand = random.randint(10, 100)
+tiempo_compra=0
+tiempo_inscripcion=0
 materiasHabilitadasArr = [
     "Algebra I", "Calculo I", "Introduccion a la programacion", "Metodologia de la investigacion", "Fisica General",
     "Ingles", "Algebra II",
@@ -74,7 +75,7 @@ if __name__ == '__main__':
         "materiasDeseadas": materias_deseadas,
         "codSys": cod_sys,
         "boleta": False,
-        "billetera": 250,
+        "billetera": billeteraRand,
     })
     cajero = run_agent(bcolors.WARNING + nombre_cajero + bcolors.ENDC, attributes={
         "monto": 14
@@ -107,62 +108,63 @@ if __name__ == '__main__':
     if cajero.get_attr("monto") <= estudiante.get_attr("billetera"):
         monto_restante = abs(estudiante.get_attr("billetera") - cajero.get_attr("monto"))
         estudiante.set_attr(billetera=monto_restante, boleta=True)
+
+        time.sleep(1)
+
+        # El director y el estudiante realizan una conexion para el paso de mensajes
+
+        director.connect(dirEst, handler=log_message)
+        estudiante.connect(dirDir, handler=log_message)
+
+        # Inscripcion de materias
+
+        print(bcolors.HEADER + '  (ESTUDIANTE)  Hola Director, deseo inscribirme a las siguientes materias: ' + str(
+            estudiante.get_attr("materiasDeseadas")) + bcolors.ENDC)
+        time.sleep(1)
+        print(bcolors.WARNING + '  (DIRECTOR)  Hola estudiante necesito tu codigo Sis y tu boleta' + bcolors.ENDC)
+        time.sleep(1)
+        tiempo_inscripcion = random.randint(5, 15)
+        aumentar_tiempo(tiempo_inscripcion)
+        if estudiante.get_attr("boleta"):
+            print(bcolors.HEADER + '  (ESTUDIANTE)  Mi codigo Sys es: ' + str(
+                estudiante.get_attr("codSys")) + ' y tengo el recibo de mi boleta aqui' + bcolors.ENDC)
+            materiasTomadas = [x for x in director.get_attr("materiasRecibidas") if
+                               x in estudiante.get_attr("materiasDeseadas")]
+            if len(materiasTomadas) >= 1:
+                # El director inscribe a los estudiantes
+                time.sleep(1)
+                todos_los_estudiantes = director.get_attr("estudiantes")
+                todos_los_estudiantes.append({
+                    "codSys": estudiante.get_attr("codSys"),
+                    "boleta": estudiante.get_attr("boleta"),
+                    "materiasInscritas": materiasTomadas
+
+                }, )
+                director.set_attr(estudiantes=todos_los_estudiantes)
+                print(bcolors.WARNING + '  (DIRECTOR)  Te inscribi con exito en tus materias!!' + bcolors.ENDC)
+                materiasUltimoEstudiante = todos_los_estudiantes[len(todos_los_estudiantes) - 1]
+                print(bcolors.OKBLUE + "(------SISTEMA------)  El director inscribio al estudiante en las siguientes "
+                                       "materias: " + bcolors.ENDC
+                      , *materiasUltimoEstudiante["materiasInscritas"], sep='      \n- ')
+            else:
+                time.sleep(1)
+                print(
+                    bcolors.WARNING + '  (DIRECTOR)  No existen materias habilitadas suficientes para que puedas continuar.' + bcolors.ENDC)
+        else:
+            time.sleep(1)
+            print(bcolors.WARNING + '  (DIRECTOR)  Debes pasar por caja y comprar una boleta.' + bcolors.ENDC)
+
+        print(bcolors.OKBLUE + '(------SISTEMA------) Fin Iteracion.' + bcolors.ENDC)
     else:
-        print(bcolors.OKBLUE + "  ----SISTEMA----   el estudiante no tiene dinero:" + bcolors.ENDC)
+        print(bcolors.OKCYAN + '  (CAJERO)  No puedes inscribirte por que no tienes el dinero suficiente'+ bcolors.ENDC)
     print(bcolors.OKBLUE + "   ----SISTEMA----   el estudiante tiene en la billetera: " + str(
         estudiante.get_attr("billetera")) + bcolors.ENDC)
     aumentar_tiempo(tiempo_compra)
 
-    time.sleep(1)
-
-    # El director y el estudiante realizan una conexion para el paso de mensajes
-
-    director.connect(dirEst, handler=log_message)
-    estudiante.connect(dirDir, handler=log_message)
-
-    # Inscripcion de materias
-
-    print(bcolors.HEADER + '  (ESTUDIANTE)  Hola Director, deseo inscribirme a las siguientes materias: ' + str(
-        estudiante.get_attr("materiasDeseadas")) + bcolors.ENDC)
-    time.sleep(1)
-    print(bcolors.WARNING + '  (DIRECTOR)  Hola estudiante necesito tu codigo Sis y tu boleta' + bcolors.ENDC)
-    time.sleep(1)
-    tiempo_inscripcion = random.randint(5, 15)
-    aumentar_tiempo(tiempo_inscripcion)
-    if estudiante.get_attr("boleta"):
-        print(bcolors.HEADER + '  (ESTUDIANTE)  Mi codigo Sys es: ' + str(
-            estudiante.get_attr("codSys")) + ' y tengo el recibo de mi boleta aqui' + bcolors.ENDC)
-        materiasTomadas = [x for x in director.get_attr("materiasRecibidas") if
-                           x in estudiante.get_attr("materiasDeseadas")]
-        if len(materiasTomadas) >= 1:
-            # El director inscribe a los estudiantes
-            time.sleep(1)
-            todos_los_estudiantes = director.get_attr("estudiantes")
-            todos_los_estudiantes.append({
-                "codSys": estudiante.get_attr("codSys"),
-                "boleta": estudiante.get_attr("boleta"),
-                "materiasInscritas": materiasTomadas
-
-            }, )
-            director.set_attr(estudiantes=todos_los_estudiantes)
-            print(bcolors.WARNING + '  (DIRECTOR)  Te inscribi con exito en tus materias!!' + bcolors.ENDC)
-            materiasUltimoEstudiante = todos_los_estudiantes[len(todos_los_estudiantes) - 1]
-            print(bcolors.OKBLUE + "(------SISTEMA------)  El director inscribio al estudiante en las siguientes "
-                                   "materias: " + bcolors.ENDC
-                  , *materiasUltimoEstudiante["materiasInscritas"], sep='      \n- ')
-        else:
-            time.sleep(1)
-            print(
-                bcolors.WARNING + '  (DIRECTOR)  No existen materias habilitadas suficientes para que puedas continuar.' + bcolors.ENDC)
-    else:
-        time.sleep(1)
-        print(bcolors.WARNING + '  (DIRECTOR)  Debes pasar por caja y comprar una boleta.' + bcolors.ENDC)
-
-    print(bcolors.OKBLUE + '(------SISTEMA------) Fin Iteracion.' + bcolors.ENDC)
 
     tiempo_acumulado = tiempo_compra + tiempo_inscripcion + tiempo_habilitacion_materias
     tiempo_con_formato = convertir(tiempo_acumulado)
-    print(bcolors.OKBLUE + '(------SISTEMA------) El proceso de inscripcion duro: '+str(tiempo_con_formato)+ bcolors.ENDC)
+    print(bcolors.OKBLUE + '(------SISTEMA------) El proceso de inscripcion duro un total de: '+str(tiempo_con_formato)+ bcolors.ENDC)
     print(" - "+str(convertir(tiempo_habilitacion_materias))+" Durante el la habililtacion de materias")
     print(" - "+str(convertir(tiempo_compra))+" Durante el tiempo de compra")
     print(" - "+str(convertir(tiempo_inscripcion))+" Durante el tiempo de inscripcion")
